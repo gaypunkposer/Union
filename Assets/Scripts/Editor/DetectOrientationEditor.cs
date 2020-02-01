3c9ca438b8b15edb3e1005528ea691fb7aee4cca
Jessica Coan <jcoan188@gmail.com>
2020-01-31 23:10:14 -0700

    Imported Yarn, device orientation

---
+++ Assets/Scripts/Editor/DetectOrientationEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DetectOrientation))]
+public class DetectOrientationEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        DetectOrientation orientation = (DetectOrientation) target;
+        if (GUILayout.Button("Rotate Landscape"))
+        {
+            orientation.enabled = false;
+            orientation.onRotateLandscape.Invoke();
+        }
+
+        if (GUILayout.Button("Rotate Portrait"))
+        {
+            orientation.enabled = false;
+            orientation.onRotatePortrait.Invoke();
+        }
+    }
+}
