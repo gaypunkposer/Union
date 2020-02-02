2ad3438059a8a306264837d03c9d33c11d8ae772
Jessica Coan <jcoan188@gmail.com>
2020-02-02 14:48:32 -0700

    Finalizing game

---
+++ Assets/Scripts/ForcePortrait.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcePortrait : MonoBehaviour
+{
+    void Start()
+    {
+        SetPortrait();
+    }
+
+    public void SetPortrait()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+}
