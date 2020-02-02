af0d0427de8740d707682c76417d2a3b512ec3d4
tptrw61 <tptrw.61@gmail.com>
2020-02-02 14:11:02 -0700

    started select screen

---
+++ Assets/Scripts/ButtonDialogue.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonDialogue : MonoBehaviour
+{
+	public Button btn;
+	public void Start()
+	{
+		btn.onClick.AddListener(TaskOnClick);
+	}
+	public void TaskOnClick()
+	{
+		SceneManager.LoadScene("Dialogue");
+	}
+}
---
+++ Assets/Scripts/ButtonMinigame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonMinigame : MonoBehaviour
+{
+	public Button btn;
+ 	public void Start()
+ 	{
+ 		btn.onClick.AddListener(TaskOnClick);
+ 	}
+
+    public void TaskOnClick()
+    {
+	    SceneManager.LoadScene("Minigame");
+    }
+}
