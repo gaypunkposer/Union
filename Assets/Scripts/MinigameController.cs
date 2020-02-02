55912f529ff8ffccd5c4c9661ab347c6a19bc8d0
JupiterDoomsday <jupiterdoomsday@gmail.com>
2020-02-01 23:31:13 -0700

    made EnterTrigger2d() event

--- Assets/Scripts/MinigameController.cs
+++ Assets/Scripts/MinigameController.cs
@@ -117,18 +117,9 @@ public class MinigameController : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.collider == faxCol) {
-            if (collision.gameObject.GetType() == PaperFax) {
-
-            }
-        }
-
-            else if (collision.collider == faxCol) {
-
-            Debug.Log(collision.gameObject.GetType());
-        }
+            Debug.Log(other.gameObject.GetType());
     }
 
 
